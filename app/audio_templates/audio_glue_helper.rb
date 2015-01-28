ed5b6b4cb953f017075cc68e4f0bf74df7f29e61
morganric <morganric@gmail.com>
2015-01-28 23:02:40 +0000

    bundle

--- app/audio_templates/audio_glue_helper.rb
+++ app/audio_templates/audio_glue_helper.rb
@@ -4,31 +4,31 @@ module AudioGlueHelper
 # of +.glue+ templates.
 # Methods are supposed to be based upon the +file+ and +url+ methods.
 	
-  def say(text)
-    url("http://some-speaking-service.com/say/#{text}")
-  end
+  # def say(text)
+  #   url("http://some-speaking-service.com/say/#{text}")
+  # end
 
 
-   # Doesn't support :url snippets, only files in local file system.
-  # Doesn't handle rate and channels.
-  class SimpleAdapter < AudioGlue::BaseAdapter
-    # Only this method is required to be implemented.
-    def build(snippet_packet)
-      # Extract file paths from snippets.
-      # Ensure only :file snippets are present.
-      file_paths = snippet_packet.snippets.map do |snippet|
-        unless snippet.type == :file
-          raise(AudioGlue::Error, "Only file snippets are supported")
-        end
-        snippets.source
-      end
+  #  # Doesn't support :url snippets, only files in local file system.
+  # # Doesn't handle rate and channels.
+  # class SimpleAdapter < AudioGlue::BaseAdapter
+  #   # Only this method is required to be implemented.
+  #   def build(snippet_packet)
+  #     # Extract file paths from snippets.
+  #     # Ensure only :file snippets are present.
+  #     file_paths = snippet_packet.snippets.map do |snippet|
+  #       unless snippet.type == :file
+  #         raise(AudioGlue::Error, "Only file snippets are supported")
+  #       end
+  #       snippets.source
+  #     end
 
-      # Build cat command
-      command = "cat #{file_paths.join(' ')}"
+  #     # Build cat command
+  #     command = "cat #{file_paths.join(' ')}"
 
-      # Concatenate files and return result
-      %{command}
-    end
-  end
+  #     # Concatenate files and return result
+  #     %{command}
+  #   end
+  # end
 
 end
