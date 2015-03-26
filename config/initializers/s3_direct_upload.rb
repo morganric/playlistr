5d686d633e26717e15b6450afd0394a594b907ce
morganric <morganric@gmail.com>
2015-03-26 17:03:59 +0000

    key

--- config/initializers/s3_direct_upload.rb
+++
@@ -1,7 +0,0 @@
-S3DirectUpload.config do |c|
-  c.access_key_id = ""       # your access key id
-  c.secret_access_key = ""   # your secret access key
-  c.bucket = "playlistrapp"              # your bucket name
-  c.region = 'us-west-2'              # region prefix of your bucket url. This is _required_ for the non-default AWS region, eg. "s3-eu-west-1"
-  c.url = "https://playlistrapp.s3.amazonaws.com/"                # S3 API endpoint (optional), eg. "https://#{c.bucket}.s3.amazonaws.com/"
-end
